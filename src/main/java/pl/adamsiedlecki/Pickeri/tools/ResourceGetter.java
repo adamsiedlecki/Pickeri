ab48772132669a8907ff060d8ea5a52d8e764dc9
adam <adamsiedlec@wp.pl>
2019-08-31 14:16:41 +0200

    Status: everythings works, but:  * on Linux, while executing JAR (not using IntelliJ) there is a problem with logo file (FileNotFound)  * reversing slashes in image path does't work  * few other strategies, for example use of Guava doesn't work

--- src/main/java/pl/adamsiedlecki/Pickeri/tools/ResourceGetter.java
+++ src/main/java/pl/adamsiedlecki/Pickeri/tools/ResourceGetter.java
@@ -1,6 +1,10 @@
 package pl.adamsiedlecki.Pickeri.tools;
 
+
 import java.io.File;
+import java.net.URL;
+
+import com.google.common.io.Resources;
 
 public class ResourceGetter {
 
@@ -10,7 +14,26 @@ public class ResourceGetter {
             if (osName.indexOf("win") >= 0) {
                 logo = new File("src\\main\\resources\\images\\pickeri.png");
             } else if (osName.indexOf("nux") >= 0) {
-                logo = new File("src/main/resources/images/pickeri.png");
+                URL url = Resources.getResource("/images/pickeri.png");
+                logo = new File(url.getFile());
+                if(!logo.exists()){
+                    logo = new File("src/main/resources/images/pickeri.png");
+                }
+                if(!logo.exists()){
+                    URL url2 = Resources.getResource("images/pickeri.png");
+                    logo = new File(url2.getFile());
+                }
+                if(!logo.exists()){
+                    logo = new File("src/main/resources/images/pickeri.png");
+                }
+                if(!logo.exists()){
+                    logo = new File("*.png");
+                }
+                if(!logo.exists()){
+                    while(true){
+                        System.out.println("ERROR: PICKERI LOGO NOT FOUND");
+                    }
+                }
 
             }
             return logo;
