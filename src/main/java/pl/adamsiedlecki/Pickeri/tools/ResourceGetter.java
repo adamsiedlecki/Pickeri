f2f4ffe60132b0a32a5f187cf21df90b604cf2c1
adam <adamsiedlec@wp.pl>
2019-09-18 12:57:33 +0200

    I have improved ResourceGetter class and now it does not repeat redundant operations.

--- src/main/java/pl/adamsiedlecki/Pickeri/tools/ResourceGetter.java
+++ src/main/java/pl/adamsiedlecki/Pickeri/tools/ResourceGetter.java
@@ -8,138 +8,48 @@ import com.google.common.io.Resources;
 public class ResourceGetter {
 
     public static File getPickeriLogo(){
-            File logo = null;
-            String osName = System.getProperty("os.name").toLowerCase();
-            if (osName.indexOf("win") >= 0) {
-                logo = new File("src\\main\\resources\\images\\pickeri.png");
-            } else if (osName.indexOf("nux") >= 0) {
-                //URL url = Resources.getResource("pickeri.png");
-                //logo = new File(url.getFile());
-                logo = new File("pickeri.png");
-                if(!logo.exists()){
-                    logo = new File("src/main/resources/images/pickeri.png");
-                }
-                if(!logo.exists()){
-                    URL url2 = Resources.getResource("images/pickeri.png");
-                    logo = new File(url2.getFile());
-                }
-                if(!logo.exists()){
-                    logo = new File("src/main/resources/images/pickeri.png");
-                }
-                if(!logo.exists()){
-                    logo = new File("pickeri.png");
-                }
-                if(!logo.exists()){
-                    URL url = Resources.getResource("/images/pickeri.png");
-                    logo = new File(url.getFile());
-                }
-                if(!logo.exists()){
-                    while(true){
-                        System.out.println("ERROR: PICKERI LOGO NOT FOUND");
-                    }
-                }
-
-            }
-            return logo;
+            return getFileByName("pickeri.png");
     }
 
     public static File getSiedleckiBlackLogo(){
-            File logo = null;
-            String osName = System.getProperty("os.name").toLowerCase();
-            if (osName.indexOf("win") >= 0) {
-                logo = new File("src\\main\\resources\\images\\logo2.png");
-            } else if (osName.indexOf("nux") >= 0) {
-                //URL url = Resources.getResource("pickeri.png");
-                //logo = new File(url.getFile());
-                logo = new File("logo2.png");
-                if(!logo.exists()){
-                    logo = new File("src/main/resources/images/logo2.png");
-                }
-                if(!logo.exists()){
-                    URL url2 = Resources.getResource("images/logo2.png");
-                    logo = new File(url2.getFile());
-                }
-                if(!logo.exists()){
-                    logo = new File("src/main/resources/images/logo2.png");
-                }
-                if(!logo.exists()){
-                    logo = new File("logo2.png");
-                }
-                if(!logo.exists()){
-                    URL url = Resources.getResource("/images/logo2.png");
-                    logo = new File(url.getFile());
-                }
-                if(!logo.exists()){
-                    while(true){
-                        System.out.println("ERROR: SIEDLECKI LOGO NOT FOUND");
-                    }
-                }
-
-            }
-            return logo;
+            return getFileByName("logo2.png");
     }
 
     public static File getSiedleckiWhiteLogo(){
-        File logo = null;
-        String osName = System.getProperty("os.name").toLowerCase();
-        if (osName.indexOf("win") >= 0) {
-            logo = new File("src\\main\\resources\\images\\logo9.png");
-        } else if (osName.indexOf("nux") >= 0) {
-            logo = new File("logo9.png");
-            if(!logo.exists()){
-                logo = new File("src/main/resources/images/logo9.png");
-            }
-            if(!logo.exists()){
-                URL url2 = Resources.getResource("images/logo9.png");
-                logo = new File(url2.getFile());
-            }
-            if(!logo.exists()){
-                logo = new File("src/main/resources/images/logo9.png");
-            }
-            if(!logo.exists()){
-                logo = new File("logo9.png");
-            }
-            if(!logo.exists()){
-                URL url = Resources.getResource("/images/logo9.png");
-                logo = new File(url.getFile());
-            }
-            if(!logo.exists()){
-                while(true){
-                    System.out.println("ERROR: SIEDLECKI LOGO NOT FOUND");
-                }
-            }
-
-        }
-        return logo;
+        return getFileByName("logo9.png");
     }
 
     public static File getTractorPicture(){
+        return getFileByName("trzydziestka.jpg");
+    }
+
+    public static File getFileByName(String  fileNameWithExtension){
         File logo = null;
         String osName = System.getProperty("os.name").toLowerCase();
         if (osName.indexOf("win") >= 0) {
-            logo = new File("src\\main\\resources\\images\\trzydziestka.jpg");
+            logo = new File("src\\main\\resources\\images\\"+fileNameWithExtension);
         } else if (osName.indexOf("nux") >= 0) {
             logo = new File("trzydziestka.jpg");
             if(!logo.exists()){
-                logo = new File("src/main/resources/images/trzydziestka.jpg");
+                logo = new File("src/main/resources/images/"+fileNameWithExtension);
             }
             if(!logo.exists()){
-                URL url2 = Resources.getResource("images/trzydziestka.jpg");
+                URL url2 = Resources.getResource("images/"+fileNameWithExtension);
                 logo = new File(url2.getFile());
             }
             if(!logo.exists()){
-                logo = new File("src/main/resources/images/trzydziestka.jpg");
+                logo = new File("src/main/resources/images/"+fileNameWithExtension);
             }
             if(!logo.exists()){
-                logo = new File("trzydziestka.jpg");
+                logo = new File(fileNameWithExtension);
             }
             if(!logo.exists()){
-                URL url = Resources.getResource("/images/trzydziestka.jpg");
+                URL url = Resources.getResource("/images/"+fileNameWithExtension);
                 logo = new File(url.getFile());
             }
             if(!logo.exists()){
                 while(true){
-                    System.out.println("ERROR: TRACTOR PICTURE NOT FOUND");
+                    System.out.println("ERROR: "+fileNameWithExtension+" NOT FOUND");
                 }
             }
 
