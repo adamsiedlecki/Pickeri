c4e0f3e0128955dad0f9b171743114e2ab60fbc6
adam <adamsiedlec@wp.pl>
2019-10-21 14:37:43 +0200

    I have added USD price in PLN display.

--- src/main/java/pl/adamsiedlecki/Pickeri/tools/ApiReader/InfoGetter.java
+++ src/main/java/pl/adamsiedlecki/Pickeri/tools/ApiReader/InfoGetter.java
@@ -1,14 +1,19 @@
 package pl.adamsiedlecki.Pickeri.tools.ApiReader;
 
+import elemental.json.JsonArray;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 
 import java.io.IOException;
 
 public class InfoGetter {
+
+    private static final Logger log = LoggerFactory.getLogger(InfoGetter.class);
 
     public static String getBitcoinPrice(Environment env){
         String url = env.getProperty("bitcoin.api");
@@ -19,12 +24,26 @@ public class InfoGetter {
             String price = offer.get(0).toString();
             return price;
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error(e.getMessage());
         } catch (JSONException e) {
-            e.printStackTrace();
+            log.error(e.getMessage());
         }
         return "";
     }
 
+    public static String getDollarPrice(Environment env){
+        String url = env.getProperty("dollar.api");
+        try {
+            JSONObject object = JsonReader.readJsonFromUrl(url);
+            JSONArray array = (JSONArray) object.get("rates");
+            JSONObject rate = array.getJSONObject(0);
+            return  Double.toString((Double) rate.get("bid"));
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        } catch (JSONException e) {
+            log.error(e.getMessage());
+        }
+        return "";
+    }
 
 }
--- src/main/java/pl/adamsiedlecki/Pickeri/web/components/StockInfoPanel.java
+++ src/main/java/pl/adamsiedlecki/Pickeri/web/components/StockInfoPanel.java
@@ -3,11 +3,6 @@ package pl.adamsiedlecki.Pickeri.web.components;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
-import elemental.json.JsonArray;
-import elemental.json.JsonObject;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,15 +10,6 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import pl.adamsiedlecki.Pickeri.tools.ApiReader.InfoGetter;
-import pl.adamsiedlecki.Pickeri.tools.ApiReader.JsonReader;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
 
 
 @Component
@@ -32,7 +18,8 @@ public class StockInfoPanel extends VerticalLayout {
 
     private Environment env;
     private Logger log = LoggerFactory.getLogger(StockInfoPanel.class);
-    private Label priceLabel;
+    private Label btcPriceLabel;
+    private Label dollarPriceLabel;
 
 
     @Autowired
@@ -40,15 +27,18 @@ public class StockInfoPanel extends VerticalLayout {
         this.env = env;
         setWidth(100, Unit.PERCENTAGE);
         setHeight(50, Unit.PIXELS);
-        priceLabel = new Label();
-        this.addComponent(priceLabel);
+        btcPriceLabel = new Label();
+        dollarPriceLabel = new Label();
+        this.addComponents(btcPriceLabel, new Label(), new Label(), dollarPriceLabel);
         refreshPrice();
         this.forEach(component -> {this.setComponentAlignment(component, Alignment.MIDDLE_CENTER);});
         this.addLayoutClickListener(event -> refreshPrice());
     }
 
     private void refreshPrice(){
-            priceLabel.setValue(InfoGetter.getBitcoinPrice(env) +" PLN");
-            priceLabel.setCaption(env.getProperty("bitcoin.price"));
+        btcPriceLabel.setValue(InfoGetter.getBitcoinPrice(env) +" PLN");
+        btcPriceLabel.setCaption(env.getProperty("bitcoin.price"));
+        dollarPriceLabel.setValue(InfoGetter.getDollarPrice(env)+" PLN");
+        dollarPriceLabel.setCaption(env.getProperty("dollar.price"));
     }
 }
