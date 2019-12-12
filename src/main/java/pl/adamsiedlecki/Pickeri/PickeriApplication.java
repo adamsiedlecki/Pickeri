436e6a9a09dfd74124473f9357b3471be6ed199b
adam <adamsiedlec@wp.pl>
2019-12-12 10:44:17 +0100

    I have added presentation branch with expiration date.

--- src/main/java/pl/adamsiedlecki/Pickeri/PickeriApplication.java
+++ src/main/java/pl/adamsiedlecki/Pickeri/PickeriApplication.java
@@ -1,14 +1,25 @@
 package pl.adamsiedlecki.Pickeri;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+
 @SpringBootApplication
-    public class PickeriApplication {
+public class PickeriApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(PickeriApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(PickeriApplication.class, args);
-
+        LocalDate now = LocalDate.now();
+        LocalDate expirationDate = LocalDate.of(2020, 8, 1);
+        if(now.isBefore(expirationDate)){
+            SpringApplication.run(PickeriApplication.class, args);
+        }else{
+            log.info("APP VERSION EXPIRED ;(");
+        }
     }
 
 }
