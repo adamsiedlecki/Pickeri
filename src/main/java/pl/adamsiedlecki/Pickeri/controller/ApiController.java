ef7004978fa26ed1f2ffb3f452b61af15f2f8709
adam <adamsiedlec@wp.pl>
2020-05-24 22:24:27 +0200

    I have added a way to download a pickers raport via api

--- src/main/java/pl/adamsiedlecki/Pickeri/controller/ApiController.java
+++ src/main/java/pl/adamsiedlecki/Pickeri/controller/ApiController.java
@@ -6,12 +6,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.adamsiedlecki.Pickeri.entity.FruitPicker;
 import pl.adamsiedlecki.Pickeri.service.*;
+import pl.adamsiedlecki.Pickeri.tools.pdf.PickersToPdfWriter;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -20,6 +29,7 @@ public class ApiController {
 
     private final FruitPickerService fruitPickerService;
     private final FruitDeliveryService fruitDeliveryService;
+    private final FruitTypeService fruitTypeService;
     private static final Logger log = LoggerFactory.getLogger(ApiController.class);
     private final Environment env;
 
@@ -30,7 +40,7 @@ public class ApiController {
         this.env = environment;
         this.fruitPickerService = pickerService;
         this.fruitDeliveryService = fruitDeliveryService;
-
+        this.fruitTypeService = fruitTypeService;
     }
 
     @GetMapping(value = "/get-all/{key}")
@@ -56,6 +66,32 @@ public class ApiController {
     @GetMapping(value = "/get-today-deliveries/{key}")
     public List<?> getTodayDeliveries(@PathVariable String key) {
         return getDataStrings(key, fruitDeliveryService.getTodayDeliveries());
+    }
+
+    @GetMapping(value = "/download-pickers-pdf/{key}", produces = "application/pdf")
+    public ResponseEntity<InputStreamResource> downloadPickersPdf(@PathVariable String key) throws IOException {
+        String pass = env.getProperty("api.pass");
+        if (pass.equals(key)) {
+            String pdfPath = "src\\main\\resources\\downloads\\pickersRaport.pdf";
+            String fileName = "pickersRaport.pdf";
+            File check = new File(pdfPath);
+            if (check.exists()) {
+                check.delete();
+            }
+            List<FruitPicker> fruitPickers = fruitPickerService.findAll();
+            PickersToPdfWriter.writeRaport(fruitPickers, pdfPath, fruitTypeService, env);
+
+            log.info("Calling Download:- " + fileName);
+            File file = new File("src\\main\\resources\\downloads\\" + fileName);
+            InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION,
+                            "attachment;filename=" + file.getName())
+                    .contentType(MediaType.APPLICATION_PDF).contentLength(file.length())
+                    .body(resource);
+        } else {
+            return null;
+        }
     }
 
     private String getDataStrings(String key, Number getNumberFunction) {
