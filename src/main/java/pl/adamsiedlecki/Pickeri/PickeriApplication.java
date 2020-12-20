ed84f2c191189b6b902205ff530a4f85663aeeb2
adam <adamsiedlec@wp.pl>
2020-12-20 14:02:31 +0100

    I have deleted expiration mechanism

--- src/main/java/pl/adamsiedlecki/Pickeri/PickeriApplication.java
+++ src/main/java/pl/adamsiedlecki/Pickeri/PickeriApplication.java
@@ -1,25 +1,13 @@
 package pl.adamsiedlecki.Pickeri;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class PickeriApplication {
 
-    private static final Logger log = LoggerFactory.getLogger(PickeriApplication.class);
-
     public static void main(String[] args) {
-        LocalDate now = LocalDate.now();
-        LocalDate expirationDate = LocalDate.of(2021, 8, 1);
-        if(now.isBefore(expirationDate)){
-            SpringApplication.run(PickeriApplication.class, args);
-        }else{
-            log.info("APP VERSION EXPIRED ;(");
-        }
+        SpringApplication.run(PickeriApplication.class, args);
     }
 
 }
